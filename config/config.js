a9949a4c8e46b8103a07145158a9b35d3629d25a
youngprinnce <ajiboyeadedotun16@gmail.com>
2021-01-30 20:17:09 +0100

    code config file

---
+++ config/config.js
@@ -0,0 +1,11 @@
+const config ={
+    serverPort: 3000,
+    profile:{
+        name: "Ajiboye Adedotun",
+        github: "@youngprinnce",
+        email: "ajiboyeadedotun16@gmail.com",
+        mobile: "08154267564",
+    }
+}
+
+module.exports = config;
